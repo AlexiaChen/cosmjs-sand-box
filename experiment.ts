1ea3880ab012578c032ead7367a7b11c010bed11
MathxH Chen <brainfvck@foxmail.com>
2022-11-26 17:55:11 +0800

    Update

--- experiment.ts
+++ experiment.ts
@@ -1,7 +1,18 @@
-import { IndexedTx, StargateClient } from "@cosmjs/stargate"
+import { readFile } from "fs/promises"
+import { DirectSecp256k1HdWallet, OfflineDirectSigner } from "@cosmjs/proto-signing"
+import { IndexedTx, SigningStargateClient, StargateClient } from "@cosmjs/stargate"
 import { Tx } from "cosmjs-types/cosmos/tx/v1beta1/tx"
+import { MsgSend } from "cosmjs-types/cosmos/bank/v1beta1/tx"
+
+
 
 const rpc = "rpc.sentry-01.theta-testnet.polypore.xyz:26657"
+
+const getAliceSignerFromMnemonic = async (): Promise<OfflineDirectSigner> => {
+    return DirectSecp256k1HdWallet.fromMnemonic((await readFile("./testnet.alice.mnemonic.key")).toString(), {
+        prefix: "cosmos",
+    })
+}
 
 const runAll = async(): Promise<void> => {
     console.log("TODO")
@@ -22,6 +33,43 @@ const runAll = async(): Promise<void> => {
     console.log("DecodedTx:", decodedTx)
     console.log("Decoded messages:", decodedTx.body!.messages)
 
+    const sendMessage: MsgSend = MsgSend.decode(decodedTx.body!.messages[0].value)
+    console.log("Sent message:", sendMessage)
+
+    const faucet: string = sendMessage.fromAddress
+    console.log("Faucet balances:", await client.getAllBalances(faucet))
+
+    const rawLog = JSON.parse(faucetTx.rawLog)
+    console.log("Raw log:", JSON.stringify(rawLog, null, 4))
+
+    const faucet_another: string = rawLog[0].events
+    .find((eventEl: any) => eventEl.type === "coin_spent")
+    .attributes.find((attribute: any) => attribute.key === "spender").value
+
+    console.log("Faucet balances in another way:", await client.getAllBalances(faucet_another))
+
+    const aliceSigner: OfflineDirectSigner = await getAliceSignerFromMnemonic()
+    const alice = (await aliceSigner.getAccounts())[0].address
+    console.log("Alice's address from signer", alice)
+    const signingClient = await SigningStargateClient.connectWithSigner(rpc, aliceSigner)
+    console.log(
+        "With signing client, chain id:",
+        await signingClient.getChainId(),
+        ", height:",
+        await signingClient.getHeight()
+    )
+
+    console.log("Gas fee:", decodedTx.authInfo!.fee!.amount)
+    console.log("Gas limit:", decodedTx.authInfo!.fee!.gasLimit.toString(10))
+    console.log("Alice balance before:", await client.getAllBalances(alice))
+    console.log("Faucet balance before:", await client.getAllBalances(faucet))
+    const result = await signingClient.sendTokens(alice, faucet, [{ denom: "uatom", amount: "100000" }], {
+        amount: [{ denom: "uatom", amount: "500" }],
+        gas: "200000",
+    })
+    console.log("Transfer result:", result)
+    console.log("Alice balance after:", await client.getAllBalances(alice))
+    console.log("Faucet balance after:", await client.getAllBalances(faucet))
 }
 
 
